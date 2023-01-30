0fb69f9110a130a57fd2e68838a5dde03c457a11
abraham <abraham@newverveconsulting.com>
2023-01-30 21:02:08 +0000

    typo fixing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /**
- * A simple express app to connect to a MongoDB 
+ * A simple express app to connect to a MongoDB
  * in just 3 steps !
  *
  * STEP 1
@@ -7,15 +7,15 @@
  * docker run --name mongo-00 -d -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=mongoadmin -e MONGO_INITDB_ROOT_PASSWORD=secret mongo
  *
  * connection string being: mongodb://mongoadmin:secret@localhost:27017
- * 
+ *
  * STEP 2
- * go to the folder were you have downloaded this code and run this on your command line:
+ * go to the folder where you have downloaded this code and run this on your command line:
  * yarn
- * 
+ *
  * STEP 2
  * in the same folder just run:
  * yarn start
- * 
+ *
  * Good luck have fun!
  * Do you want to know more? https://github.com/bezkoder/node-express-mongodb
  * To manage users and roles in mongodb: https://docs.mongodb.com/manual/tutorial/manage-users-and-roles/
@@ -29,17 +29,17 @@ const mongoose = require('mongoose');
 const dbName = 'AbriTest' // if the database doesn't exists, then it will be created
 mongoose.Promise = global.Promise; // is this really needed?
 mongoose.connect(`mongodb://localhost:27017/${dbName}?authSource=admin`, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    // to connecto with authSource=admin you need to login with below user + pass
-    user: 'mongoadmin',
-    pass: 'secret',
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  // to connecto with authSource=admin you need to login with below user + pass
+  user: 'mongoadmin',
+  pass: 'secret',
 }).then(() => {
-    console.log(`successfully connected to database ${dbName}`);
+  console.log(`successfully connected to database ${dbName}`);
 }).catch(err => {
-    console.log(`error connecting to database ${dbName}`);
-    console.log(err);
-    process.exit();
+  console.log(`error connecting to database ${dbName}`);
+  console.log(err);
+  process.exit();
 });
 
 const kittySchema = new mongoose.Schema({
@@ -55,14 +55,14 @@ app.use(
   express.urlencoded({ extended: true })
 ); /* bodyParser.urlencoded() is deprecated */
 
-async function saveKitten () {
+async function saveKitten() {
   const silence = new Kitten({ name: 'Silence' });
   console.log(silence.name); // 'Silence'
   await silence.save();
 };
 // saveKitten();
 
-async function getKitten () {
+async function getKitten() {
   const res = await Kitten.find();
   console.log(res);
   return res;
@@ -82,7 +82,7 @@ app.get("/add", async (req, res) => {
   await saveKitten();
   const kittens = await Kitten.find();
   console.log(kittens)
-  res.json({ 
+  res.json({
     message: "Kitten added, these are all your cats",
     kittens: kittens,
   });
@@ -92,7 +92,7 @@ app.get("/add", async (req, res) => {
 app.get("/get", async (req, res) => {
   const kittens = await getKitten();
   console.log(kittens)
-  res.json({ 
+  res.json({
     message: "All Kittens",
     kittens: kittens,
   });
@@ -101,7 +101,7 @@ app.get("/get", async (req, res) => {
 app.get("/delete", async (req, res) => {
   const isDeleted = await deleteAllData();
   console.log(isDeleted)
-  res.json({ 
+  res.json({
     message: "All Kittens",
     kittens: isDeleted,
   });
@@ -109,18 +109,18 @@ app.get("/delete", async (req, res) => {
 
 // simple route
 app.get("/json", (req, res) => {
-  res.json({ 
+  res.json({
     message: "Welcome to bezkoder application.",
     try: "adding index to the URL above",
   });
 });
 
-app.get('/index', function(req,res) {
+app.get('/index', function (req, res) {
   // { root: '.' } will set you one folder up
   res.sendFile('index.html', { root: '.' });
 });
 
-app.get('/', function(req,res) {
+app.get('/', function (req, res) {
   // { root: '.' } will set you one folder up
   res.sendFile('index.html', { root: '.' });
 });
